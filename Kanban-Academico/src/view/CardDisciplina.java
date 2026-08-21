396991443f394b97c5515b24c4954e36ba76479a
Sidney Silva <sidneysilva0907@gmail.com>
2026-08-21 15:19:47 -0300

    Add CardDisciplina class for displaying discipline info

--- Kanban-Academico/src/view/CardDisciplina.java
+++ Kanban-Academico/src/view/CardDisciplina.java
@@ -1,1 +1,26 @@
+package view;
 
+import model.Disciplina;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CardDisciplina extends JPanel {
+
+    public CardDisciplina(Disciplina disciplina){
+
+        setLayout(new GridLayout(3,1));
+
+        setBackground(new Color(170,220,255));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        setPreferredSize(new Dimension(220,90));
+        setMaximumSize(new Dimension(220,90));
+
+        add(new JLabel("Disciplina: " + disciplina.getNome()));
+        add(new JLabel("Professor: " + disciplina.getProfessor().getNome()));
+        add(new JLabel("Alunos: " + disciplina.getQuantidadeAlunos()));
+
+    }
+
+}
