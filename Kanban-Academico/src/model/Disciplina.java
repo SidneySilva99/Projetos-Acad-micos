f18621b4de8593b245848ae08f068e95ff9017aa
Sidney Silva <sidneysilva0907@gmail.com>
2026-08-21 15:16:11 -0300

    Implement student management in Disciplina class
    Added methods to manage students in the Disciplina class.

--- Kanban-Academico/src/model/Disciplina.java
+++ Kanban-Academico/src/model/Disciplina.java
@@ -1,1 +1,41 @@
+package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Disciplina {
+    private String nome;
+    private Professor professor;
+
+    public Disciplina(String nome, Professor professor) {
+        this.nome = nome;
+        this.professor = professor;
+        this.alunos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    private ArrayList<Aluno> alunos = new ArrayList<>();
+
+    public void adicionarAluno(Aluno aluno){
+        alunos.add(aluno);
+    }
+
+    public void removerAluno(Aluno aluno){
+        alunos.remove(aluno);
+    }
+
+    public int getQuantidadeAlunos(){
+        return alunos.size();
+    }
+
+    public String toString() {
+        return nome + " - Prof: " + professor.getNome() + "";
+    }
+}
